05449c4cc5cc57decfbd1581e0296edf8aeef6e7
Viktor Berke <github.bviktor@outlook.com>
2023-02-09 17:55:31 +0100

    Pull before build and skip cache

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 export PLATFORM=$(echo $1 | cut -d'/' -f1)
 export RELEASE=$(echo $1 | cut -d'/' -f2)
 
-docker build --tag "bviktor/ansible-systemd-${PLATFORM}:${RELEASE}" "${1}"
+docker build --no-cache --pull --tag "bviktor/ansible-systemd-${PLATFORM}:${RELEASE}" "${1}"
