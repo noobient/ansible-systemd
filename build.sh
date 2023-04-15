59b0d5ae9a0c8eca669b67bf298884a2ec76a106
Viktor Berke <github.bviktor@outlook.com>
2023-04-15 12:52:36 +0200

    Actual error handling and stuff

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PLATFORM=$(echo $1 | cut -d'/' -f1)
-export RELEASE=$(echo $1 | cut -d'/' -f2)
-
-docker build --no-cache --pull --tag "bviktor/ansible-systemd-${PLATFORM}:${RELEASE}" "${1}"
--- push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PLATFORM=$(echo $1 | cut -d'/' -f1)
-export RELEASE=$(echo $1 | cut -d'/' -f2)
-
-docker push "bviktor/ansible-systemd-${PLATFORM}:${RELEASE}"
